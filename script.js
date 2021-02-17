a7d42cf7001268e758997698dcb84a026fc030bc
kk0055 <rtsw.lgy@gmail.com>
2021-02-17 11:04:33 +0800

    JS for planfolia

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const body = document.querySelector('.img-box')
 const slides = document.querySelectorAll('.slide')
-const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
+
+const slidesPlan = document.querySelectorAll('.slide-planifolia')
+const rightBtnForPlan = document.getElementById('right2')
+
+
 
 let activeSlide = 0
 
@@ -14,11 +18,30 @@ rightBtn.addEventListener('click', () => {
 
  
   setActiveSlide()
+
 })
 
+rightBtnForPlan.addEventListener('click', () => {
+  activeSlide++
 
+  if (activeSlide > slidesPlan.length - 1) {
+    activeSlide = 0
+  }
+
+  setActiveSlideForPlan()
+
+})
+
+//Groumet
 function setActiveSlide() {
   slides.forEach((slide) => slide.classList.remove('active'))
 
   slides[activeSlide].classList.add('active')
+}
+
+//Planifolia
+function setActiveSlideForPlan() {
+  slidesPlan.forEach((slide) => slide.classList.remove('active'))
+
+  slidesPlan[activeSlide].classList.add('active')
 }
